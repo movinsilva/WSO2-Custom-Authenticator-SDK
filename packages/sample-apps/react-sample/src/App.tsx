16a74d094ff86e6a2a5276279f6ae51398593f36
unknown <movinpinsara@gmail.com>
2024-02-13 09:52:46 +0530

    rollup configs, packages connected

--- packages/sample-apps/react-sample/src/App.tsx
+++ packages/sample-apps/react-sample/src/App.tsx
@@ -1,9 +1,10 @@
-
+import { BasicAuthenticationForm } from "asgardeo-react";
 function App() {
 
   return (
     <>
       Hello! Welcome to the sample app!!!
+      <BasicAuthenticationForm baseUrl='https://localhost:9443' clientId='CtYxaqN68OXg0a1sWrLcfARALxIa' scope='openid internal_login' redirectUri='http://localhost:8080' flowId='908b60c9-fe96-419a-82b2-0323a98baada' authenticatorId='QmFzaWNBdXRoZW50aWNhdG9yOkxPQ0FM'/>
     </>
   )
 }
