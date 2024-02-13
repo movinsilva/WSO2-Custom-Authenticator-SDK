adb96849a01d89c904cc2ba09d7818a7803698d0
unknown <movinpinsara@gmail.com>
2024-02-13 10:00:42 +0530

    comments

--- packages/core/src/data/config-data.js
+++ packages/core/src/data/config-data.js
@@ -1,6 +1,14 @@
 import { AuthenticationCoreConfig } from '../core/authentication-core-config.js';
 import dataLayer from './data-layer.js';
 
+
+/**
+ * Sets the authentication configuration.
+ * @param {string} baseUrl - The base URL of the authentication server.
+ * @param {string} clientId - The client ID for authentication.
+ * @param {string} scope - The scope of the authentication.
+ * @param {string} redirectUri - The redirect URI for authentication.
+ */
 const authConfig = (baseUrl, clientId, scope, redirectUri) => {
   const config = new AuthenticationCoreConfig(baseUrl, clientId, scope, redirectUri);
   dataLayer.set('authConfig', config);
