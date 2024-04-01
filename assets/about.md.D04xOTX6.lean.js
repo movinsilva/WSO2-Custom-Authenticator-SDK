2cc7d2336eb85abd8ce7e4c472b98a2cfcd0439e
movinsilva <movinsilva@users.noreply.github.com>
2024-04-01 04:04:29 +0000

    deploy: 499a9af16a4cad8295ad9c6fe93d702b72b6216b

---
+++ assets/about.md.D04xOTX6.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,a2 as o}from"./chunks/framework.BB8kWMIj.js";const _=JSON.parse('{"title":"About","description":"","frontmatter":{},"headers":[],"relativePath":"about.md","filePath":"about.md"}'),r={name:"about.md"},s=o("",7),n=[s];function p(i,u,d,c,l,h){return a(),e("div",null,n)}const g=t(r,[["render",p]]);export{_ as __pageData,g as default};
